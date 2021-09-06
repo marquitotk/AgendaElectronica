15f1d45e302aecf8a0dbcf3a4c96e738be92b192
marquitotk <marquitotk@hotmail.com>
2021-09-06 00:28:17 -0500

    Add files via upload

---
+++ categorias.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        
+    <title>.:Agenda Electronica:.</title>
+    <link rel="icon" type="image/png" href="public/imagenes/logo.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php require_once "dependencias.php"; ?>
+</head>
+<body>
+    <div class="container" >
+        <?php require_once "menu.php"?>
+    
+    </div>
+    
+</body>
+</html>
